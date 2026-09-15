67284c90095f017514c24c0620e139dc0aed0ccb
Dhani_Baksh <generativeaiandchatbot@gmail.com>
2026-09-15 16:27:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,13 @@ app.add_middleware(
     expose_headers=["*"],
     max_age=600,
 )
+@app.middleware("http")
+async def add_cors_headers(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS, PATCH"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
 
 @app.options("/{rest_of_path:path}")
 async def options_handler(rest_of_path: str):
